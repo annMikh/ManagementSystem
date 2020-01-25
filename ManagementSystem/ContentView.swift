d10df05983f1eda697eb01d3396aab4a46b5f57f
Anya <mihalewa2015@yandex.ru>
2020-01-25 20:19:53 +0300

    Initial Commit

---
+++ ManagementSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ManagementSystem
+//
+//  Created by Анна Михалева on 25.01.2020.
+//  Copyright © 2020 Анна Михалева. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
