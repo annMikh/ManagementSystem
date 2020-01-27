8de219d09e5cf01b72b021bc35bf72a6c4d3d162
Anya <mihalewa2015@yandex.ru>
2020-01-27 10:56:50 +0300

    add picker to registration

---
+++ ContentViews/CreateProjectScreen.swift
@@ -0,0 +1,9 @@
+//
+//  CreateProjectScreen.swift
+//  ManagementSystem
+//
+//  Created by Анна Михалева on 26.01.2020.
+//  Copyright © 2020 Анна Михалева. All rights reserved.
+//
+
+import Foundation
